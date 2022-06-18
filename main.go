41e123b962dcd20315d02405c456aa1bf475ff1d
s33h0w <theo.s33h0w@gmail.com>
2022-06-18 18:27:37 +0800

    add fag page

--- main.go
+++ main.go
@@ -15,6 +15,18 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"http://mailto:theo.s33h0w@gmail.com\">theo.s33h0w@gmail.com</a></p>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ</h1>
+	<ul>
+		<li><b>How are you?</b> I'm fine.</li>
+		<li><b>How are you?</b> I'm fine.</li>
+		<li><b>How are you?</b> I'm fine.</li>
+		<li><b>How are you?</b> I'm fine.</li>
+	</ul>
+	`)
+}
+
 type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +35,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	}
