b33810baed6e690d925fb524bbf99443e891e97d
s33h0w <theo.s33h0w@gmail.com>
2022-06-18 18:17:09 +0800

    extract http.Handler

--- main.go
+++ main.go
@@ -15,7 +15,9 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"http://mailto:theo.s33h0w@gmail.com\">theo.s33h0w@gmail.com</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -27,7 +29,6 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", new(Router))
 }
