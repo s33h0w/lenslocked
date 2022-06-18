88ea7c137d63926282aae84c185fb990a2af1558
s33h0w <theo.s33h0w@gmail.com>
2022-06-18 16:38:55 +0800

    custom routing

--- main.go
+++ main.go
@@ -15,9 +15,21 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"http://mailto:theo.s33h0w@gmail.com\">theo.s33h0w@gmail.com</a></p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// TODO: handle the page not found error
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Not Found</h1>")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
