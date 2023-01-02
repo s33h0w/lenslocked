405634480df5fd2131c1667c12f97b9918b61f11
s33h0w <theo.s33h0w@gmail.com>
2023-01-02 17:14:53 +0800

    add postgres config to models

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/s33how/lenslocked/models"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
-}
-
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "baloo",
-		Password: "junglebook",
-		Database: "lenslocked",
-		SSLMode:  "disable",
-	}
-	db, err := sql.Open("pgx", cfg.String())
+	cfg := models.DefaultPostGresConfig()
+	db, err := models.Open(cfg)
 	if err != nil {
 		panic(err)
 	}
---
+++ models/postgres.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type PostGresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostGresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
+func DefaultPostGresConfig() PostGresConfig {
+	return PostGresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+}
+
+// Open will open a SQL connection with the provided
+// Postgres database. Callers of Open need to ensure
+// the connection is eventually closed via the
+// db.Close() method.
+func Open(config PostGresConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", config.String())
+	if err != nil {
+		return nil, fmt.Errorf("open: %w", err)
+	}
+	return db, nil
+}
