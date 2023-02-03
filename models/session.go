a96b8355a39eebbf0e2ad8f693f6996043cad358
s33h0w <theo.s33h0w@gmail.com>
2023-02-03 18:14:58 +0800

    hash session

--- models/session.go
+++ models/session.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/s33how/lenslocked/rand"
@@ -28,9 +30,9 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	}
 	// TODO: Hash the session token
 	session := Session{
-		UserID: userID,
-		Token:  token,
-		// TODO: Set the TokenHash
+		UserID:    userID,
+		Token:     token,
+		TokenHash: ss.hash(token),
 	}
 	// TODO: Store the session in the database
 	return &session, nil
@@ -39,4 +41,9 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 func (ss *SessionService) User(token string) (*User, error) {
 	// TODO: Implement SessionService.User
 	return nil, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
